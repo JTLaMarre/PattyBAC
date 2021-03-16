5812ebd1b7a67566ddfcbdeadeb91051ddd63751
Jacob LaMarre <lordaufchaos@yahoo.com>
2021-03-16 10:48:08 -0400

    oops

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/BAC'));
+app.use(express.static(__dirname + '/dist/patty-bac'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/BAC/index.html'));
+res.sendFile(path.join(__dirname+'/dist/patty-bac/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
