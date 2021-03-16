cdb9b26c9da3f120bb4f5420e0587c99d53a437e
Jacob LaMarre <lordaufchaos@yahoo.com>
2021-03-16 14:21:41 -0400

    trying to deploy still

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/patty-bac'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/patty-bac/index.html'));
+res.sendFile(path.join(__dirname+'/patty-bac/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
