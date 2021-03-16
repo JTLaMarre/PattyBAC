983ceb4a460a31b260a4d2af6e3ec6009127e8f9
Jacob LaMarre <lordaufchaos@yahoo.com>
2021-03-16 14:10:30 -0400

    trying not sending the dist

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/patty-bac'));
+app.use(express.static(__dirname + '/patty-bac'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/patty-bac/index.html'));
+res.sendFile(path.join(__dirname+'/patty-bac/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
-// dist
+// Trying no dist?
+
